712361b8aaeaa9bd445b91a493e0ad4105b89e86
RathodKeshma <55710322+RathodKeshma@users.noreply.github.com>
2019-10-08 15:42:45 -0400

    Adding the Dependencies.sh file for adding all the dependencies.

---
+++ Dependencies.sh
@@ -0,0 +1,17 @@
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+
+python get-pip.py
+
+pip install pandas
+
+pip install spacy
+
+pip install scipy
+
+pip install matplotlib
+ 
+pip3 install torch==1.2.0+cpu torchvision==0.4.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+
+pip install allennlp
+
+  
